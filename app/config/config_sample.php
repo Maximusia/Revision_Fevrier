7a9a462ad68826cb86b7bdf8898c54973bf57ce8
Maximusia <tsarandromaximusia@email.com>
2026-02-11 21:31:53 +0000

    Modificatio : partie database

--- app/config/config_sample.php
+++ app/config/config_sample.php
@@ -67,10 +67,12 @@ return [
 	 **************************************/
 	'database' => [
 		// MySQL Example:
-		// 'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
-		// 'dbname'   => 'your_db_name',   // Database name (e.g., 'flightphp')
-		// 'user'     => 'your_username',  // Database user (e.g., 'root')
-		// 'password' => 'your_password',  // Database password (never commit real passwords)
+		'driver' => 'pgsql',
+		'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
+		'port'     => '5432',             // Database port (e.g., 3306 for MySQL, 5432 for PostgreSQL)
+		'dbname'   => 'takalo',   // Database name (e.g., 'flightphp')
+		'user'     => 'root',  // Database user (e.g., 'root')
+		'password' => '',  // Database password (never commit real passwords)
 
 		// SQLite Example:
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite', // Path to SQLite file
