8ee30879fa2a57d7c8d995dba423910c283bb695
Maximusia <tsarandromaximusia@email.com>
2026-02-11 20:35:28 +0000

    ajout : table users

--- app/utils/sql/table.sql
+++ app/utils/sql/table.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE takalo;
 
 \c takalo;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    mdp VARCHAR(255) NOT NULL
+);
